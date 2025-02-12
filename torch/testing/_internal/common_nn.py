85d599fa1713135036756aa7f35d75a86af65482
fan.mo <fan.mo@mthreads.com>
2025-02-12 10:11:36 +0800

    update align with glu

--- torch/testing/_internal/common_nn.py
+++ torch/testing/_internal/common_nn.py
@@ -2668,7 +2668,7 @@ def get_new_module_tests():
     non_linear_activations_no_batch = [
         'ELU', 'Hardshrink', 'Hardsigmoid', 'Hardtanh', 'Hardswish', 'LeakyReLU',
         'LogSigmoid', 'PReLU', 'ReLU', 'ReLU6', 'RReLU', 'SELU', 'CELU', 'GELU', 'GLU',
-        'Sigmoid', 'SiLU', 'Mish', 'Softplus', 'Softshrink', 'Softsign', 'Tanh',
+        'Sigmoid', 'SiLU', 'Mish', 'Softplus', 'Softshrink', 'Softsign', 'SwiGLU', 'Tanh',
         'Tanhshrink', 'Threshold'
     ]
     non_linear_activations_extra_info: Dict[str, dict] = {
@@ -2696,6 +2696,7 @@ def get_new_module_tests():
         'Softplus': {'default_dtype': torch.double},
         'Softshrink': {'default_dtype': torch.double},
         'Softsign': {'default_dtype': torch.double},
+        'SwiGLU': {'default_dtype': torch.double},
         'Tanh': {'default_dtype': torch.double},
         'Tanhshrink': {'default_dtype': torch.double},
     }
